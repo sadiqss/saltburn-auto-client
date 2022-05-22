5bed38ec473fa6eaf1574aea1d13f51e46d6717d
sadiqss <sadiq.sau48@gmail.com>
2022-05-22 01:31:04 +0100

    basic project setup

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Banner from './Banner';
+import BusinessSummary from './BusinessSummary';
+import Parts from './Parts';
+import Reviews from './Reviews';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Saltburn Auto Solution</h1>
+            <Banner></Banner>
+            <Parts></Parts>
+            <BusinessSummary></BusinessSummary>
+            <Reviews></Reviews>
+        </div>
+    );
+};
+
+export default Home;
