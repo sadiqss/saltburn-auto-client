d5bebb48efec45512dcc346065a0a39241c97bd3
sadiqss <sadiq.sau48@gmail.com>
2022-05-24 20:37:04 +0100

    data loaded from mongodb

--- src/pages/Home/Parts.js
+++ src/pages/Home/Parts.js
@@ -1,70 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Part from './Part';
-import engine from '../../assets/parts/engine.jpg';
-import wheel from '../../assets/parts/wheel.jpg';
-import transmission from '../../assets/parts/transmission.jpg';
-import battery from '../../assets/parts/battery.jpg';
-import radiator from '../../assets/parts/radiator.jpg';
-import steering from '../../assets/parts/steering.jpg';
 import { Link } from 'react-router-dom';
 
 const Parts = () => {
-    const parts = [
-        {
-            "_id": 1,
-            "name": "Engine",
-            "img": engine,
-            "description": "",
-            "minimumOrder": 2,
-            "available": 1000,
-            "price": 400
-        },
-        {
-            "_id": 2,
-            "name": "Wheel",
-            "img": wheel,
-            "description": "",
-            "minimumOrder": 2,
-            "available": 1000,
-            "price": 400
-        },
-        {
-            "_id": 3,
-            "name": 'transmission',
-            "img": transmission,
-            "description": "",
-            "minimumOrder": 2,
-            "available": 1000,
-            "price": 400
-        },
-        {
-            "_id": 4,
-            "name": "Battery",
-            "img": battery,
-            "description": "",
-            "minimumOrder": 2,
-            "available": 1000,
-            "price": 400
-        },
-        {
-            "_id": 5,
-            "name": "Radiator",
-            "img": radiator,
-            "description": "",
-            "minimum-order": 2,
-            "available": 1000,
-            "price": 400
-        },
-        {
-            "_id": 6,
-            "name": "Front Steering and Suspension",
-            "img": steering,
-            "description": "",
-            "minimumOrder": 2,
-            "available": 1000,
-            "price": 400
-        }
-    ]
+    const [parts, setParts] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/parts')
+            .then(res => res.json())
+            .then(data => setParts(data));
+    }, [])
 
     return (
         <div className='my-28'>
