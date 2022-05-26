89b06384615f52ec00b04bccec6946329eb46b1f
sadiqss <sadiq.sau48@gmail.com>
2022-05-26 02:57:57 +0100

    user specific dashboard

--- src/pages/Dashboard/MyPurchase.js
+++ src/pages/Dashboard/MyPurchase.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyPurchase = () => {
+    const [orders, setOrders] = useState([]);
+    const [user] = useAuthState(auth);
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/order?buyer=${user.email}`)
+                .then(res => res.json())
+                .then(data => setOrders(data));
+        }
+    }, [user])
     return (
         <div>
-            <h1>Ny purchase </h1>
+            <h2>My appointment:{orders.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Product</th>
+                            <th>Quantity</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map((a, index) => <tr>
+                                <th>{index + 1}</th>
+                                <td>{a.buyerName}</td>
+                                <td>{a.part}</td>
+                                <td>{a.quantity}</td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
